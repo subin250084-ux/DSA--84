6fd3bbfef024497587cfaaa0531dbc2eddfcf9d6
subin250084-ux <subin.250084@csbs.ritchennai.edu.in>
2026-09-10 11:15:58 +0530

    2b

---
+++ subincsbs5.py
@@ -0,0 +1,13 @@
+def fibonacci(n):
+    if n <= 1:
+        return n
+    else:
+        return fibonacci(n - 1) + fibonacci(n - 2)
+
+
+n = int(input("Enter the number of terms: "))
+
+print("Fibonacci Series:")
+for i in range(n):
+    print(fibonacci(i), end=" ")
+
